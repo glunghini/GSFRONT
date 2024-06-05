0a21169784615ad703b1c8b9197ab9081663a603
André Flores <andreluizsf16@gmail.com>
2024-06-05 11:52:40 -0300

    login feito!!!!!!!!!!!!!!!!!!!

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,12 @@
-console.log("xd")
+function logar(){
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+    
+    if(login == "admin" && senha == "123"){
+        alert('Sucesso')
+    }
+    else{
+        alert('Usuário ou senha incorretos');
+    }
+}
+
